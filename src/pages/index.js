a71f0c1188998e83004bed9c1848d4893a284649
Kenny Heagle <kenneth.heagle@bluejeans.com>
2023-08-02 10:10:46 -0700

    added image to homepage - gatsby tutorial part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from "react"
 import Layout from "./layout";
+import { StaticImage } from "gatsby-plugin-image"; 
 
 const IndexPage = () => {
   return (
     <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+          alt="The surface of the moon, provided by NASA"
+          src="../images/nasa-moon.jpg"
+        />
       </Layout>
     </main>
   )
