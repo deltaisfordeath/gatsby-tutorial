26ca6ac5687548fdd986421ed0f38cad88b9e8b3
Kenny Heagle <kenneth.heagle@bluejeans.com>
2023-08-09 13:53:30 -0700

    gatsby tutorial part 4 - added queries and blog page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
+  siteTitle,
   heading,
   navLinks,
   navLinkItem,
@@ -9,8 +10,20 @@ import {
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -21,6 +34,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
